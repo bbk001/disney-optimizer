fb7bb12f8202a34f09d7b4f54d569c54807736bb
Eric Thompson-Martin <ethompsonmartin@g.hmc.edu>
2022-04-24 09:45:34 -0700

    remove trailing / in api base url

--- disney-optimizer-frontend/src/utils/consts.js
+++ disney-optimizer-frontend/src/utils/consts.js
@@ -1,4 +1,4 @@
-export const api_base_url = 'http://disney-optimizer.herokuapp.com/api/'
+export const api_base_url = 'http://disney-optimizer.herokuapp.com/api'
 
 export const parkOpen = {h: 8, mi: 0}
 export const parkClose = {h: 23, mi: 50}
